304a4bb3fdd106036973c0c2a52eb2221281659a
Ali Qasimzade <ali@globalsoft.az>
2023-02-27 01:23:02 +0400

    fixed

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,65 +3,73 @@ import Cart from "./pages/Cart/Cart";
 import Checkout from "./pages/Checkout/Checkout";
 import WishList from "./pages/Wishlist/WishList";
 import AllCategories from "./pages/AllCategories/AllCategories";
-import Login from "./pages/Login/Login"
+import Login from "./pages/Login/Login";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import Products from "./pages/admin/Pages/Products/Products";
 import Orders from "./pages/admin/Pages/Orders/Orders";
 import Users from "./pages/admin/Pages/Users/Users";
 import AdminLayout from "./pages/admin/AdminLayout";
 import Admins from "./pages/admin/Pages/Admins/Admins";
+import Layout from "../Layout";
 
 const routes = [
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/wishlist",
-    element: <WishList />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/checkout",
-    element: <Checkout />
-  },
-  {
-    path: "/all-categories",
-    element: <AllCategories />,
-  },
-  {
-    path:"/login",
-    element: <Login />
-  },
-  {
-    path:"/products/:_id",
-    element:<ProductDetail />
-  },
-  {
-    path:"/admin",
-    element:<AdminLayout />,
+    element: <Layout />,
     children: [
       {
-        index:true,
-        element:<Admins />
+        index: true,
+        element: <Home />,
       },
       {
-        path:"products",
-        element:<Products />
+        path: "wishlist",
+        element: <WishList />,
       },
       {
-        path:"orders",
-        element:<Orders />
+        path: "cart",
+        element: <Cart />,
       },
       {
-        path:"users",
-        element:<Users />
-      }
-    ]
-  }
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "all-categories",
+        element: <AllCategories />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "products/:_id",
+        element: <ProductDetail />,
+      },
+
+      {
+        path: "admin",
+        element: <AdminLayout />,
+        children: [
+          {
+            index: true,
+            element: <Admins />,
+          },
+          {
+            path: "products",
+            element: <Products />,
+          },
+          {
+            path: "orders",
+            element: <Orders />,
+          },
+          {
+            path: "users",
+            element: <Users />,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default routes;
