32e9786454c5227fe740d2f1c89ecebc9c8a8473
Ali Qasimzade <ali@globalsoft.az>
2023-02-26 17:26:28 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
   build: {
-    minify: false
-  }
+    commonjsOptions: {
+      defaultIsModuleExports(id) {
+        const module = require(id);
+        if (module?.default) {
+          return false;
+        }
+        return "auto";
+      },
+    },
+  },
+  plugins: [react()],
 })
